a1f443ac5a09caca86beec3c5894190863a877ea
Alexander Shoshiashvili <alexander.shoshiashvili@appsfactory.de>
2020-04-14 16:01:58 +0200

    Add one more test file

---
+++ WasteReduction/WasteReduction/Extensions/OneMoreTest.swift
@@ -0,0 +1,13 @@
+//
+//  OneMoreTest.swift
+//  WasteReduction
+//
+//  Created by Alexander Shoshiashvili on 14.04.2020.
+//  Copyright © 2020 Junction. All rights reserved.
+//
+
+import UIKit
+
+class OneMoreTest: NSObject {
+
+}
