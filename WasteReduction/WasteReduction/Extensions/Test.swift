8691c04ce1687f9a64d74b64a537e269010516fe
Alexander Shoshiashvili <alexander.shoshiashvili@appsfactory.de>
2020-04-14 16:01:28 +0200

    Add test file

---
+++ WasteReduction/WasteReduction/Extensions/Test.swift
@@ -0,0 +1,13 @@
+//
+//  Test.swift
+//  WasteReduction
+//
+//  Created by Alexander Shoshiashvili on 14.04.2020.
+//  Copyright © 2020 Junction. All rights reserved.
+//
+
+import UIKit
+
+class Test: NSObject {
+
+}
